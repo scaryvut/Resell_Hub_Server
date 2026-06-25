48c13c8a32ecb1c2487e4f9bea1bc59df647fc2f
Arafatullah Arshad <arafatullaharshad@gmail.com>
2026-06-25 18:31:16 +0600

    add server middleware

---
+++ middleware/verifyToken.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (
+  req,
+  res,
+  next
+) => {
+  const authHeader =
+    req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({
+      message: "Unauthorized Access",
+    });
+  }
+
+  const token =
+    authHeader.split(" ")[1];
+
+  jwt.verify(
+    token,
+    process.env.JWT_SECRET,
+    (err, decoded) => {
+      if (err) {
+        return res.status(401).send({
+          message: "Invalid Token",
+        });
+      }
+
+      req.decoded = decoded;
+      next();
+    }
+  );
+};
+
+module.exports = verifyToken;
